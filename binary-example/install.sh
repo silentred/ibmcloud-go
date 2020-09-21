40fba4fdad6fad12ca76b38815eb0bacfe5afc49
shenmu.wy <shenmu.wy@antfin.com>
2020-09-21 19:53:00 +0800

    fix bug

--- binary-example/install.sh
+++ binary-example/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 read -p "请输入应用程序名称:" APPNAME
 read -p "请设置你的容器内存大小(默认256):" RAMSIZE
 
@@ -5,7 +7,7 @@ CURDIR=`pwd`
 DIRNAME=$HOME/binary-example
 V2RAY_TMPDIR="v2ray-tmp"
 UUID=`cat /proc/sys/kernel/random/uuid`
-PATH=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
+WSPATH=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
 
 if [ -z "$RAMSIZE" ];then
     RAMSIZE=256
@@ -26,5 +28,6 @@ mv $DIRNAME/$V2RAY_TMPDIR/v2ray $DIRNAME/v2ray
 mv $DIRNAME/$V2RAY_TMPDIR/v2ctl $DIRNAME/v2ctl
 rm -rf $DIRNAME/$V2RAY_TMPDIR
 
-echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$UUID'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$PATH'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}' > $DIRNAME/config.json
+echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$UUID'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$WSPATH'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}' > $DIRNAME/config.json
 
+cp $CURDIR/manifest.yml $DIRNAME/manifest.yml
