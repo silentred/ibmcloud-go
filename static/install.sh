d12302496def43a205b120f46b00ab5ccd39cd73
shenmu.wy <shenmu.wy@antfin.com>
2020-09-22 20:22:04 +0800

    change

--- static/install.sh
+++ static/install.sh
@@ -8,12 +8,14 @@ WSPATH=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
 
 wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip -d $V2RAY_TMPDIR v2ray-linux-64.zip
-chmod 777 $V2RAY_TMPDIR/*
+chmod 600 $V2RAY_TMPDIR/*
 
 rm -rf v2ray-linux-64.zip
-mv $DIRNAME/$V2RAY_TMPDIR/v2ray $DIRNAME/v2ray.txt
-mv $DIRNAME/$V2RAY_TMPDIR/v2ctl $DIRNAME/v2ctl.txt
+mv $DIRNAME/$V2RAY_TMPDIR/v2ray $DIRNAME/v2ray
+mv $DIRNAME/$V2RAY_TMPDIR/v2ctl $DIRNAME/v2ctl
 rm -rf $DIRNAME/$V2RAY_TMPDIR
 
-echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$UUID'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$WSPATH'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}' > $DIRNAME/test.txt
+echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$UUID'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$WSPATH'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}' > $DIRNAME/config.txt
+
+chmod 600 $DIRNAME/config.txt
 
